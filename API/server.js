02beccab7a69733b8e5a8e202b890656edaf4318
Riley Zhang <ryyizg8@gmail.com>
2023-04-02 19:49:36 -0700

    some more API endpoints

--- API/server.js
+++ API/server.js
@@ -33,8 +33,109 @@ router.use(function (request, response, next) {
     console.log("REQUEST:" + request.method + "   " + request.url);
     console.log("BODY:" + JSON.stringify(request.body));
     response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     response.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
+
+/* POSTS ENDPOINTS*/
+
+// Create a new Visitor (INSERT)
+router.route('/visitor').post(function (request, response) {
+    console.log("CREATE POST");
+    oracledb.getConnection(connectionProperties, function (err, connection) {
+        if (err) {
+            console.error(err.message);
+            response.status(500).send("Error connecting to DB");
+            return;
+        }
+        console.log("After connection");
+
+        // TODO: make sure this reflects actual stuff from frontend
+        visitor = request.body;
+
+        connection.execute("INSERT INTO Visitor (IP, EXPERIENCE, VID, TIME)" + 
+        "VALUES(:ip, :experience, vid:, time:", [visitor.ip, visitor.experience, visitor.vid, visitor.time],
+            { outFormat: oracledb.OBJECT }, 
+            function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    response.status(500).send("Error creating post");
+                    doRelease(connection);
+                    return;
+                }
+                response.end();
+                doRelease(connection);
+            });
+    });
+});
+
+// Delete a visitor account (DELETE)
+router.route('/visitor/:vid').delete(function (request, response) {
+    console.log("DELETE VISITOR ID:" + request.params.id);
+    oracledb.getConnection(connectionProperties, function (err, connection) {
+        if (err) {
+            console.error(err.message);
+            response.status(500).send("Error connecting to DB");
+            return;
+        }
+
+        var body = request.body;
+        var id = request.params.vid;
+        connection.execute("DELETE FROM Visitor WHERE VID = :vid",
+            [vid],
+            function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    response.status(500).send("Error deleting visitor from DB");
+                    doRelease(connection);
+                    return;
+                }
+                response.end();
+                doRelease(connection);
+            });
+    });
+});
+
+// Show only visitors with an experience threshold (Selection)
+router.route('/visitor/').get(function (request, response) {
+    console.log("GET VISITORS WITH EXPERIENCE");
+    oracledb.getConnection(connectionProperties, function (err, connection) {
+        if (err) {
+            console.error(err.message);
+            response.status(500).send("Error connecting to DB");
+            return;
+        }
+        console.log("After connection");
+        connection.execute("SELECT * FROM Visitor WHERE experience >= 1000", {},
+            { outFormat: oracledb.OBJECT },
+            function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    response.status(500).send("Error getting data from DB");
+                    doRelease(connection);
+                    return;
+                }
+                console.log("RESULTSET:" + JSON.stringify(result));
+                var visitors = [];
+                result.rows.forEach(function (element) {
+                    visitors.push({
+                        ip: element.ip, experience: element.experience, vid: element.vid, time: visitor.time
+                    });
+                }, this);
+                response.json(visitors);
+                doRelease(connection);
+            });
+    });
+});
+
+// Create a post
+
+// Edit a post (UPDATE)
+
+// Return usernames (Projection)
+
+// Return users and their posts (Join)
+
+// 
